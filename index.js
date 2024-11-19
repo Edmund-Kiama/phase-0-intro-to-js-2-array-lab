f329159e3dd20ba3aa673ff2db3ba7318bee4a64
Edmund <edmundgachanja@gmail.com>
2024-11-19 20:02:42 +0300

    Adds functions that manipulate an array

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = ( name ) => cats.push( name );
+
+const destructivelyPrependCat = ( name ) => cats.unshift( name );
+
+const destructivelyRemoveFirstCat = () => cats.shift();
+
+const destructivelyRemoveLastCat = () => cats.pop();
+
+const appendCat = ( name ) => [ ...cats, name ];
+
+const prependCat = ( name ) => [ name, ...cats ];
+
+const removeLastCat = () =>  cats.slice( 0, cats.length - 1 );
+
+const removeFirstCat = () => cats.slice( 1 );
